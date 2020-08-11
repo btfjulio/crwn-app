e7c6b80bfbd01d2cb957d6cc6c6cab283f16d906
Bruno Tostes <btfjulio@hotmail.com>
2020-08-11 19:02:44 -0300

    add style to forms

---
+++ src/pages/authentication/authentication.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./authentication.styles.scss";
+
+import SignIn from "../../components/signin/signin.component";
+
+const Authentication = () => (
+  <div className="athentication">
+    <SignIn />
+  </div>
+);
+
+export default Authentication;
